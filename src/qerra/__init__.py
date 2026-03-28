4276da02eabaabb71c00a874a05f4b83c78a5ff1
MARUSSA METOCHARAKI <marunigno@gmail.com>
2026-03-28 11:51:23 +0200

    Day 8: Update src/qerra/__init__.py to expose core engine

--- src/qerra/__init__.py
+++ src/qerra/__init__.py
@@ -1,2 +1,9 @@
-# QERRA-v2 package init
+"""
+QERRA-v2 Package
+Hybrid Quantum-Classical Ethical Decision Engine
+"""
 
+from .core import QERRA_DecisionEngine
+
+__version__ = "0.1.0"
+__all__ = ["QERRA_DecisionEngine"]
